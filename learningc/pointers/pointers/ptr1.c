32bf8cbb698620f4f7a4a087e72cb93f5fde0a8e
hoodielive <hoodielive@gmail.com>
2020-04-21 20:42:09 -0400

    lazy brat sits in her room all day sowing dolls.. lol

--- learningc/pointers/pointers/ptr1.c
+++ learningc/pointers/pointers/ptr1.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 
+void square(int *input)
+{
+	*input *= *input;
+}
+
 int main()
 {
-    int a = 100;
-    int *b = &a;
+  int a = 100;
+  int *b = &a;
 
-    a = 200;
-    printf("a  = %d\n", a);
-    printf("*b  = %d\n", *b);
+  a = 200;
+  printf("a  = %d\n", a);
+  printf("*b  = %d\n", *b);
 
-    a = 300;
-    printf("a  = %d\n", a);
-    printf("*b  = %d\n", *b);
+  a = 300;
+  printf("a  = %d\n", a);
+  printf("*b  = %d\n", *b);
 
-    int c = 6000;
-    b = &c;
-    printf("a  = %d\n", a);
-    printf("*b  = %d\n", *b);
-    return 0;
+  int c = 6000;
+  b = &c;
+  printf("a  = %d\n", a);
+  printf("*b  = %d\n", *b);
+
+	int x = 5;
+	square(&x);
+	printf("%d\n", x);
+
+	int size = 6;
+	int ages[] = { 2, 43, 63000, 23, 05, 53 };
+
+	printf("memory size of ages is : %lu\n", sizeof(ages));
+  return 0;
 }
