2b0ade83dab875ea9d5d6d0208ea97e1d9a46245
hoodielive <hoodielive@gmail.com>
2020-04-25 09:30:03 -0400

    Dynamic Memory allocation - remember to use stdbool.h

---
+++ learningc/memory_management/memory.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct
+{
+	char name[30];
+	int age;
+	bool isVerified;
+} User;
+
+User *createUser(char name[], int age, bool isVerified)
+{
+	User *newUser = malloc(sizeof(User));
+	strcpy(newUser->name, name);
+	newUser->age = age;
+	newUser->isVerified = isVerified;
+
+	return newUser;
+};
+
+int main()
+{
+
+	int size;
+	printf("How many int elements u need bro?: ");
+
+	// Scan a value into size because we are doing
+	// dynamic memory allocation.
+
+	scanf("%d", &size);
+
+	// Malloc will return a pointer.
+
+	int *arr = malloc(size * sizeof(int));
+
+	// Lets make sure that we have the memory to spare
+	// before we go all willy nilly.
+
+	if(arr == 0)
+	{
+		printf("Invalid pointer. Error allocating memory.\n");
+	}
+	else
+	{
+		printf("Youre good to go!\n");
+	}
+
+	// Note: when you pass an array to a function, it
+	// decays into a pointer. Scanf needs a pointer.
+	// You can still use array-like syntax.
+
+	for (int i=0; i < size; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Your array:\n");
+
+	for (int i=0; i < size; i++)
+	{
+		printf("arr[%d] = %d\n", i, arr[i]);
+	}
+
+	free(arr);
+
+	User *me = createUser("Law Moor", 99, false);
+	printf("Law Moor is %d years old!!\n", me->age);
+
+	return 0;
+}
