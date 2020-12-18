e4a591b9acf1c0c16aed8df799fa190e9a528131
hoodielive <hoodielive@gmail.com>
2020-12-18 13:59:11 -0500

    Playing around with C-Strings and such with Brandon.

--- noletter.c
+++ noletter.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
-	int a, A;
+	char c;
+	char a, A;
 	a = 'a';
 	A = 'A';
 
+	c = 'A';
 	a = "a";
 	A = "A";
 	printf("a is: %d and A is: %d\n", a, A);
+	printf("C is: %d ", c);
 	return 0;
 }
